3c173af365cfb60b1def4837ec3472a78c687754
Noriny <noriny.pessoa@gmail.com>
2021-12-21 11:19:15 -0300

    site de transferencia e extrato

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
-import { NgModule } from '@angular/core';
+import { NgModule,LOCALE_ID, DEFAULT_CURRENCY_CODE } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
+import { ExtratoComponent } from './extrato/extrato.component';
+import localePt from '@angular/common/locales/pt'
+import { registerLocaleData } from '@angular/common';
+import { AppRoutingModule } from './app-routing.module';
 
+registerLocaleData(localePt, 'pt');
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NovaTransferenciaComponent,
+    ExtratoComponent
   ],
   imports: [
-    BrowserModule
+
+  BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'pt'},
+    {
+      provide: DEFAULT_CURRENCY_CODE,
+      useValue: "BRL",
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
